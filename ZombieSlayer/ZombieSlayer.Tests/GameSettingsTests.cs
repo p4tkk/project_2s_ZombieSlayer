687d748ecb7675d550ae6b472121c96006f05a14
Александр Амбурцев <alexamb1002@gmail.com>
2025-05-22 11:20:12 +0300

    #33 Тест константных значений класса GameSettings

--- ZombieSlayer/ZombieSlayer.Tests/GameSettingsTests.cs
+++ ZombieSlayer/ZombieSlayer.Tests/GameSettingsTests.cs
@@ -14,5 +14,30 @@ namespace ZombieSlayer.Tests
             Parallel.For(0, 100, _ => instance = GameSettings.Instance);
             Assert.AreSame(GameSettings.Instance, instance);
         }
+        [TestMethod]
+        public void GameSpeeds_AreBalanced()
+        {
+            // Arrange
+            var settings = GameSettings.Instance;
+
+            // Act & Assert для игрока
+            int playerSpeed = settings.PlayerSpeed;
+            Assert.AreEqual(10, playerSpeed,
+                "Скорость игрока должна быть 10 для правильного баланса");
+
+            // Act & Assert для зомби
+            Assert.AreEqual(3, settings.NormalZombieSpeed,
+                "Обычные зомби должны быть медленнее игрока (speed=3)");
+            Assert.AreEqual(5, settings.SmallZombieSpeed,
+                "Маленькие зомби должны быть быстрее обычных (speed=5)");
+            Assert.AreEqual(2, settings.BigZombieSpeed,
+                "Большие зомби должны быть самыми медленными (speed=2)");
+
+            // Проверка баланса между персонажами
+            Assert.IsTrue(playerSpeed > settings.NormalZombieSpeed,
+                "Игрок должен быть быстрее обычных зомби");
+            Assert.IsTrue(settings.SmallZombieSpeed > settings.NormalZombieSpeed,
+                "Маленькие зомби должны быть быстрее обычных");
+        }
     }
 }
